0d4d6bc688c411e7f78f0b0cf6cbe4dfb4fe664d
khandya1 <63929895+khandya1@users.noreply.github.com>
2021-06-09 00:26:33 +0530

    flatten doubly linked list

---
+++ C++/Linked List/FlattenMultilevelDoublyLinkedList.cpp
@@ -0,0 +1,53 @@
+// https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
+// lc 430 Medium
+
+#include <bits/stdc++.h>
+using namespace std;
+// Definition for a Node.
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+    Node *child;
+};
+
+class Solution
+{
+public:
+    Node *flatten(Node *head)
+    {
+
+        Node *temp = head;
+
+        while (temp != NULL)
+        {
+            if (temp->child)
+            {
+                Node *node_child = temp->child;
+                if (temp->next == NULL)
+                {
+                    temp->next = node_child;
+                    node_child->prev = temp;
+                    temp->child = NULL;
+                    continue;
+                }
+                Node *next_node= temp->next; // this next node pointer is required to remove infinte loop
+
+                temp->next = node_child;
+                node_child->prev = temp;
+                temp->child = NULL;
+                while (node_child->next != NULL)
+                {
+                    node_child = node_child->next;
+                }
+                node_child->next = next_node;
+                next_node->prev = node_child;
+            }
+
+            temp = temp->next;
+        }
+        return head;
+    }
+};
